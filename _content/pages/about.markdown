87aad1bf6ac590eeeb2024038e908a094663abaf
cyberdot <contact@cyberdot.co.uk>
2017-05-10 12:58:26 +0100

    Updated site generator with latest version of Dnevnik

---
+++ _content/pages/about.markdown
@@ -0,0 +1,9 @@
+{ 
+"title": "About", 
+"description": "A new page", 
+"created": "2017-05-10T11:35:37Z", 
+"tags": ["page", "new"], 
+"slug": "about"
+}
+
+Welcome to your brand new page.
--- mix.exs
+++ mix.exs
@@ -3,30 +3,31 @@ defmodule CyberdotBlog.Mixfile do
 
   def project do
     [app: :cyberdot_blog,
-     version: "0.0.1",
-     elixir: "~> 1.1",
+     version: "0.1.0",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps()]
   end
 
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    # Specify extra applications you'll use from Erlang/Elixir
+    [extra_applications: [:logger]]
   end
 
   # Dependencies can be Hex packages:
   #
-  #   {:mydep, "~> 0.3.0"}
+  #   {:my_dep, "~> 0.3.0"}
   #
   # Or git/path repositories:
   #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:xylograph, github: "cyberdot/xylograph"}]
+    [ {:dnevnik, github: "cyberdot/dnevnik" }]
   end
 end
